875790bd032ba7993206e94d86d79159540fca49
NobleQuester <twhelgeson@gmail.com>
2020-01-11 13:41:35 -0500

    Update shooter code

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,19 +1,21 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 
-public class Shooter extends PIDSubsystem {
+public class Shooter {
 
-    private static double P = 0.03;
+    private static double P = 0.2;
     private static double I = 0;
     private static double D = 0;
-    private static double F = 4;
+    private static double F = 0;
 
     private CANSparkMax[] motors = new CANSparkMax[2];
     private CANEncoder[] encoders = new CANEncoder[2];
@@ -24,9 +26,9 @@ public class Shooter extends PIDSubsystem {
     private final int ENCODER_0 = 0,
                         ENCODER_1 = 1;
 
-    public Shooter() {
-        super("Shooter PID", P, I, D, F);
+    CANPIDController controller;
 
+    public Shooter() {
         motors[MOTOR_0] = new CANSparkMax(RobotMap.CAN.SHOOTER_1, MotorType.kBrushless);
         motors[MOTOR_1] = new CANSparkMax(RobotMap.CAN.SHOOTER_2, MotorType.kBrushless);
 
@@ -35,31 +37,49 @@ public class Shooter extends PIDSubsystem {
 
         motors[MOTOR_1].follow(motors[MOTOR_0]);
 
-        setAbsoluteTolerance(20);
-        getPIDController().setContinuous(false);
-        setOutputRange(0, 1);
+        for ( CANSparkMax motor : motors ) {
+            motor.setIdleMode(IdleMode.kCoast);
+        }
+
+        controller = motors[MOTOR_0].getPIDController();
+        updateConstants();
+
     }
 
-    @Override
+    /*
+    public void stop() {
+        controller.setReference(0, ControlType.kDutyCycle);
+    }
+    */
+    
     public void setSetpoint(double setpoint) {
-        super.setSetpoint(setpoint);
+        controller.setReference(setpoint, ControlType.kVelocity);
     }
 
-    @Override
-    protected double returnPIDInput() {
+    public double publishRPM() {
+        SmartDashboard.putNumber("Shooter RPM", encoders[ENCODER_0].getVelocity());
         return encoders[ENCODER_0].getVelocity();
     }
 
-    @Override
-    protected void usePIDOutput(double output) {
-        SmartDashboard.putNumber("PID Output", output);
-        motors[MOTOR_0].pidWrite(output);
+    public void setSpeed(double speed) {
+        motors[MOTOR_0].set(speed);
     }
 
-    @Override
-    protected void initDefaultCommand() {
-
+    public void updateConstants() {
+        controller.setOutputRange(0, 1);
+        controller.setP(SmartDashboard.getNumber("P", 0));
+        controller.setI(SmartDashboard.getNumber("I", 0));
+        controller.setD(SmartDashboard.getNumber("D", 0));
+        controller.setFF(SmartDashboard.getNumber("F", 0));
     }
 
+    public void putNumbers() {
+        SmartDashboard.putNumber("P", P);
+        SmartDashboard.putNumber("I", I);
+        SmartDashboard.putNumber("D", D);
+        SmartDashboard.putNumber("F", F);
 
+        SmartDashboard.putNumber("Shooter Speed", 0);
+        SmartDashboard.putNumber("Set Shooter RPM", 3000);
+    }
 }
