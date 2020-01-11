4f30f1cd259519ae047e9439a5c11f362debf46f
nemojn <nemo@eliworks.com>
2020-01-11 14:31:28 -0500

    updated limelight to use Command interface

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -10,14 +10,15 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-public class Limelight extends Subsystem {
+public class Limelight implements Subsystem {
 
     @Override
-    protected void initDefaultCommand() {
+    public void setDefaultCommand(Command defaultCommand) {
 
     }
 
