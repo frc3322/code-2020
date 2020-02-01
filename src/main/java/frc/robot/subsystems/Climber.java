a15e022120ca3614f0f59108793c7e0af28950e3
Aaron Gankin <agankin02@gmail.com>
2020-02-01 10:17:31 -0500

    adds climber methods

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -7,24 +7,64 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Robot.m_can;
 
 public class Climber extends SubsystemBase {
   private CANSparkMax[] motors;
-
+  private CANEncoder[] encoders;
+  DoubleSolenoid intakeExtender;
+  
   private int RAISE = 0, CLIMB = 1;
   public Climber() {
     motors[RAISE] = new CANSparkMax(m_can.CLIMBER_RAISE, MotorType.kBrushless);
     motors[CLIMB] = new CANSparkMax(m_can.CLIMBER_CLIMB, MotorType.kBrushless);
+    encoders[RAISE] = motors[RAISE].getEncoder();
+    encoders[CLIMB] = motors[CLIMB].getEncoder();
   }
-
+ //TODO need to have either mechanical hardstops or stops built in to code 
   public void raiseClimber(double speed) {
     motors[RAISE].set(speed);
+  }
+
+  public void lowerClimber(double speed) {
+    motors[RAISE].set(speed);
+  }
+
+  public void pullWinch(double speed) {
+
+    motors[CLIMB].set(speed);
+  }
+
+  public void pushWinch(double speed) {
+    motors[CLIMB].set(speed);
+  }
+
+  public void extendHook() {
+    intakeExtender.set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void retractHook() {
+    intakeExtender.set(DoubleSolenoid.Value.kReverse);
+  }
+
+  public void toggle() {
+    if (isExtended()) {
+      retractHook();
+    } else {
+      extendHook();
+    }
+  }
+
+  public boolean isExtended() {
+    return intakeExtender.get() == Value.kForward;
   }
 
   @Override
