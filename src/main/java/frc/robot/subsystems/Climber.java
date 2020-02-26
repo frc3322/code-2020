8203b71114b15e758f009e1893b0207226f4ef08
NobleQuester <twhelgeson@gmail.com>
2020-02-25 23:43:46 -0500

    Added import for LED data

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.LedData;
 
 import static frc.robot.Robot.m_can;
 
